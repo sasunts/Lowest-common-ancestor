a7a80a0bd37d7816e5020e1364f62d91c885ad2e
Vahe Sasunts <sasuntsv@tcd.ie>
2018-09-19 23:19:00 +0100

    created a tree

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
+    ( bTree,lca
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
+
+bTree :: Tree char
+bTree =
+        Node 'A'
+            (Node 'B'
+                (Node 'D' Empty Empty)
+                (Node 'E' Empty Empty)
+            )
+            (Node 'C'
+                (Node 'F' Empty Empty)
+                (Node 'G' Empty Empty)
+            )
