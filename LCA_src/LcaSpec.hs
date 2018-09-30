1d1eb0802a13d9d31e8c5fd1ea5310149429f0d9
Vahe Sasunts <sasuntsv@tcd.ie>
2018-09-30 19:51:10 +0100

    nodes of different levels test

--- LCA_src/LcaSpec.hs
+++ LCA_src/LcaSpec.hs
@@ -6,8 +6,17 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "Lowest Common Ancestor Tests" $ do
-    it "lcaPrint for a tree with nodes 4 & 5 = 2" $
-      lcaPrint bTree 4 5 `shouldBe` "2"
+    it "lcaPrint nodes of 1 level" $
+      lcaPrint bTree 2 3 `shouldBe` "1"
+
+    it "lcaPrint nodes of 2 levels" $
+        lcaPrint bTree 5 4 `shouldBe` "2"
+
+    it "lcaPrint nodes of 2 levels" $
+        lcaPrint bTree 6 7 `shouldBe` "3"
+
+    it "lcaPrint nodes of different levels" $
+        lcaPrint bTree 1 7 `shouldBe` "1"
 
     it "lcaPrint error node that doesn't exist" $
         lcaPrint bTree 8 0 `shouldBe` "Error"
@@ -15,8 +24,6 @@ main = hspec $ do
     it "lcaPrint empty tree" $
         lcaPrint emptyTree 3 1 `shouldBe` "Error"
 
-    it "lcaPrint for a tree with nodes 2 & 3 = 1" $
-        lcaPrint bTree 2 3 `shouldBe` "1"
 
     it "lca with a real tree and nodes" $
         lca bTree 4 5 `shouldBe` Right 2
