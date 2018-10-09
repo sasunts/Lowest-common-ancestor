c18398692c950c01aac9ce6082b97a40dcfb6fa6
sasunts <sasuntsv@tcd.ie>
2018-10-09 21:21:14 +0100

    messy code needs to be fixed hence why in comments

--- LCA_src/Lca.hs
+++ LCA_src/Lca.hs
@@ -8,10 +8,52 @@ import Control.Error
 --Structure I want where nodes are chars and are linked by numbers relation
 --Below node a is connected to b, c and e etc.
 -- [Node "a" [1,2,4], Node "b" [3], Node "c" [3,4], Node "d" [], Node "e" []]
+{-
+data Zero
+data Succ n
 
+data Fin n where
+    Fin0 :: Fin (Succ n)
+    FinS :: Fin n -> Fin (Succ n)
 
+data Node a n where
+      Node :: a -> [Fin n] -> Node a n
+      deriving (Eq,Show)
 
+data Vec f n where
+      Empty :: Vec f Zero
+      (:::) :: f n -> Vec f n -> Vec f (Succ n)
+      infixr 5 :::
 
+type DAG a = Vec (Node a)
+
+instance (forall m. Eq (f m)) => Eq (Vec f n)
+
+class Eq1 f where
+    eq1 :: f n -> f n -> Bool
+
+instance Eq1 f => Eq (Vec f n) where
+      Empty == Empty = True
+      (x ::: xs) == (y ::: ys) = eq1 x y && xs == ys
+
+      instance Eq a => Eq1 (Node a) where
+          eq1 = (==)
+      instance Eq1 Fin where
+          eq1 = (==)
+      instance Eq1 f => Eq1 (Vec f) where
+          eq1 = (==)
+
+      -- ghc can't derive this
+      instance Eq (Fin n) where
+          Fin0   == Fin0   = True
+          FinS i == FinS j = i == j
+          _      == _      = False
+
+class Show1 a where
+  showsPrec1 :: Int -> a n -> ShowS
+-}
+
+--data Tree a = TNode a [Tree a]  deriving Show
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
 
