323d740ef922cc32ae401c6bea3115c78d344ce3
sasunts <sasuntsv@tcd.ie>
2018-10-19 15:16:51 +0100

    new test

--- Src/test.py
+++ Src/test.py
@@ -6,8 +6,13 @@ class TestLca(unittest.TestCase):
     def test_node4_5(self):
         self.assertEqual(lca.findLCA(lca.root, 4, 5).key, 2)
 
+    def test_node6_2(self):
+        self.assertEqual(lca.findLCA(lca.root, 6, 2).key, 1)
+
     def test_node_none(self):
         self.assertEqual(lca.findLCA(None, 4, 5), None)
+
+#TODO: Add more tests 
 
 if __name__ == '__main__':
     unittest.main()
