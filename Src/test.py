ba8989f2ce3ec4a7e08cb14df0a95c30d7ec8950
sasunts <sasuntsv@tcd.ie>
2018-10-19 15:15:17 +0100

    test for none

--- Src/test.py
+++ Src/test.py
@@ -6,5 +6,8 @@ class TestLca(unittest.TestCase):
     def test_node4_5(self):
         self.assertEqual(lca.findLCA(lca.root, 4, 5).key, 2)
 
+    def test_node_none(self):
+        self.assertEqual(lca.findLCA(None, 4, 5), None)
+
 if __name__ == '__main__':
     unittest.main()
