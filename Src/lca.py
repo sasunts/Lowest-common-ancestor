f15c32821b8a53b57652e70cf70582853f29a714
sasunts <sasuntsv@tcd.ie>
2018-10-24 17:36:26 +0100

    dag struct made and work for none & same node

--- Src/lca.py
+++ Src/lca.py
@@ -2,30 +2,55 @@ class Node:
 
     def __init__(self, key):
         self.key = key
-        self.left = None
-        self.right = None
+        self.pred = []
+        self.succ = []
 
-def findLCA(root, n1, n2):
+# def findLCA(root, n1, n2):
+#
+#     if root is None:
+#         return None
+#
+#     if root.key == n1 or root.key == n2:
+#         return root
+#
+#     left_lca = findLCA(root.left, n1, n2)
+#     right_lca = findLCA(root.right, n1, n2)
+#
+#     if left_lca and right_lca:
+#         return root
+#
+#     return left_lca if left_lca is not None else right_lca
+
+def dagLCA(root,n1,n2):
 
     if root is None:
         return None
 
-
     if root.key == n1 or root.key == n2:
         return root
 
-    left_lca = findLCA(root.left, n1, n2)
-    right_lca = findLCA(root.right, n1, n2)
 
-    if left_lca and right_lca:
-        return root
 
-    return left_lca if left_lca is not None else right_lca
 
 root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
+r2 = Node(2)
+r3 = Node(3)
+r4 = Node(4)
+r5 = Node(5)
+root.succ = [r2,r3,r4,r5]
+r2.succ = [r4]
+r2.pred = [root]
+r3.succ = [r4, r5]
+r3.pred = [root]
+r4.succ = [r5]
+r4.pred = [r2,r3,root]
+r5.pred = [r3,r4,root]
+
+#binary tree nodes
+# root = Node(1)
+# root.left = Node(2)
+# root.right = Node(3)
+# root.left.left = Node(4)
+# root.left.right = Node(5)
+# root.right.left = Node(6)
+# root.right.right = Node(7)
